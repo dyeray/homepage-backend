1b02e073385301c5193842b342cfd9df9563b53a
dyeray <dyeray@gmail.com>
2022-02-14 19:47:10 +0000

    Allow configuring frontend url

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import express from 'express';
 import * as views from './views.js';
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
+const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3001';
+
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', frontendUrl);
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+  res.setHeader('Access-Control-Allow-Credentials', false);
+
+  next();
+});
 
 app.get('/', views.main);
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 })
