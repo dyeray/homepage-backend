2e8153b2c0ebecfb693c32189ab0612c2774a10e
dyeray <dyeray@gmail.com>
2022-07-13 11:39:12 +0100

    Update api to use films on db

--- src/views.js
+++ src/views.js
@@ -1,21 +1,13 @@
-import axios from 'axios';
-import * as cheerio from 'cheerio';
+import * as film from './db/film.js';
 
 export async function main(req, res) {
-    const response = await axios.get('https://www.imdb.com/list/ls016522954/');
-    const $ = cheerio.load(response.data, null, false);
-    const filmNodes = $("#main div.lister-list div.lister-item");
-    debugger;
-    var list = [];
-    filmNodes.each(function (index, element) {
-        list.push(getFilm($(element)));
-    });
-    res.json(list);
+    const films = (await film.list()).map(model => mapFilm(model));
+    res.json(films);
 }
 
-function getFilm(filmNode) {
+function mapFilm(filmModel) {
     return {
-        'title': filmNode.find('.lister-item-header a').text(),
-        'image': filmNode.find('.lister-item-image img.loadlate')[0].attribs['loadlate']
+        'title': filmModel['title'],
+        'image': filmModel['image_url']
     };
 }
